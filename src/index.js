577809ea4ba51133d63dc50a979bb98725936ba1
pradeep <pradeep.9997@gmail.com>
2024-02-15 14:59:12 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import MainContext from './CartContext/MainContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let router=createBrowserRouter([
+  //Home Page
+  {
+    path:'/', //http://localhost:3000/
+    element:<Home/>
+  },
+  //Product Page
+  {
+    path:"/product", //http://localhost:3000/product
+    element:<Product/>
+  },
+  {
+    path:'/cart',  //  ttp://localhost:3000/cart
+    element:<Cart/>
+  }
+
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MainContext>
+       <RouterProvider router={router}/>
+    </MainContext>
   </React.StrictMode>
 );
 
